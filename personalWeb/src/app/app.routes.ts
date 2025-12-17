e303bf58f87f38f92f456fca92bb2e884c972033
TNKDieguexPC <diegoaguilar20040303@gmail.com>
2025-12-17 11:04:27 -0500

    Add NavBar component and update routing with Bienvenue and NonTrouvee pages

--- personalWeb/src/app/app.routes.ts
+++ personalWeb/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Bienvenue } from './pages/bienvenue/bienvenue';
+import { NonTrouvee } from './pages/non-trouvee/non-trouvee';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Bienvenue
+    },
+    {
+        path: '**',
+        component: NonTrouvee
+    }
+];
--- personalWeb/src/app/app.ts
+++ personalWeb/src/app/app.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavBar } from "./components/nav-bar/nav-bar";
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
