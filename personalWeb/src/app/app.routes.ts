488662714eb68fb1c07d523b42e0af386cd47a13
TNKDieguexPC <diegoaguilar20040303@gmail.com>
2025-12-22 15:01:02 -0500

    Add routing and components for Connexion, Taches, and Board; update NavBar and Bienvenue page

--- personalWeb/src/app/app.routes.ts
+++ personalWeb/src/app/app.routes.ts
@@ -1,11 +1,31 @@
 import { Routes } from '@angular/router';
 import { Bienvenue } from './pages/bienvenue/bienvenue';
 import { NonTrouvee } from './pages/non-trouvee/non-trouvee';
+import { Connexion } from './pages/auth/connexion/connexion';
+import { Taches } from './pages/taches/taches';
+import { Board } from './pages/board/board';
 
 export const routes: Routes = [
+    // {
+    //     path: '',
+    //     redirectTo: '/bienvenue',
+    //     pathMatch: 'full'
+    // },
     {
         path: '',
         component: Bienvenue
+    },
+    {
+        path: 'board',
+        component:Board
+    },
+    {
+        path: 'taches',
+        component: Taches
+    },
+    {
+        path: 'connexion',
+        component: Connexion
     },
     {
         path: '**',
